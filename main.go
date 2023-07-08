d0430ae83eb13f5c0810270a4778e483943750e4
SongoMen <songomengaming@gmail.com>
2023-07-08 15:06:38 +0200

    add code status

--- main.go
+++ main.go
@@ -104,7 +104,7 @@ func (client *Client) ListenForMessages() <-chan ChatMessage {
 			default:
 				_, msg, err := client.ws.ReadMessage()
 				if err != nil {
-					if err.(*websocket.CloseError).Code == 4200 {
+					if err.(*websocket.CloseError).Code == 4200 || err.(*websocket.CloseError).Code == 4201 {
 						fmt.Println("Connection lost, Reconnecting...")
 						reconnectErr := client.reconnect()
 						if reconnectErr != nil {
